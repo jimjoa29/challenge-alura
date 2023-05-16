78e5cc11c617a95671750cd35ad91699402095fb
Joan <jimjoa2903@gmail.com>
2023-05-15 23:02:21 -0400

    agregando autofucus al html y corrigiendo logica de main js

--- main.js
+++ main.js
@@ -1,103 +1,58 @@
-const btnEncriptar = document.querySelector("#btn-encriptar"); 
+const btnEncriptar = document.querySelector("#btn-encriptar");
 const entrada = document.querySelector("#textarea");
 const salida = document.querySelector("#salida");
 const btnCopiar = document.querySelector("#btn-copiar");
 const btnDesencriptar = document.querySelector("#btn-desencriptar");
 
-const sectionOutputPresentacion = document.querySelector(".section-output-presentacion");
-const sectionOutputEncriptacion = document.querySelector(".section-output-encriptacion");
+const sectionOutputPresentacion = document.querySelector(
+  ".section-output-presentacion"
+);
+const sectionOutputEncriptacion = document.querySelector(
+  ".section-output-encriptacion"
+);
 
-btnEncriptar.addEventListener("click", () => encriptarDatos() );
+btnEncriptar.addEventListener("click", () => encriptarDatos());
 btnDesencriptar.addEventListener("click", () => desencriptarDatos());
-btnCopiar.addEventListener("click", () => copiarDatos() );
+btnCopiar.addEventListener("click", () => copiarDatos());
 
-function encriptarDatos() {        
+function encriptarDatos() {
+  if (!entrada.value == "") {
     let texto = entrada.value;
     let textoMinusculas = texto.toLowerCase();
-    let textoEncriptado = textoMinusculas.replace(/e/gi, "enter").replace(/i/gi, "imes").replace(/o/gi, "ober").replace(/u/gi, "ufat")        
-    sectionOutputPresentacion.style.display = "none";        
+    let textoEncriptado = textoMinusculas
+      .replace(/e/gim, "enter")
+      .replace(/i/gim, "imes")
+      .replace(/a/gim, "ai")
+      .replace(/o/gim, "ober")
+      .replace(/u/gim, "ufat");
+    sectionOutputPresentacion.style.display = "none";
     sectionOutputEncriptacion.style.display = "flex";
-    salida.textContent = textoEncriptado;    
+    salida.textContent = textoEncriptado;
+  } else {
+    alert("Incresa una cadena de texto por favor");
+  }
 }
 
-function desencriptarDatos() {       
+function desencriptarDatos() {
+  if (!entrada.value == "") {
     let texto = entrada.value;
     let textoMinusculas = texto.toLowerCase();
-    let textoEncriptado = textoMinusculas.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u")        
-    sectionOutputPresentacion.style.display = "none";        
+    let textoEncriptado = textoMinusculas
+      .replace(/enter/gim, "e")
+      .replace(/imes/gim, "i")
+      .replace(/ai/gim, "a")
+      .replace(/ober/gim, "o")
+      .replace(/ufat/gim, "u");
+    sectionOutputPresentacion.style.display = "none";
     sectionOutputEncriptacion.style.display = "flex";
-    salida.textContent = textoEncriptado;  
-
+    salida.textContent = textoEncriptado;
+  } else {
+    alert("Incresa una cadena de texto por favor");
+  }
 }
 
-function copiarDatos() {    
-    navigator.clipboard.writeText(salida.textContent);
-    entrada.value = ""; 
+function copiarDatos() {
+  navigator.clipboard.writeText(salida.textContent);
+  entrada.value = "";
 }
-
-
-
-
-
-
-
-
-
-
-
-               
-
-
-// btnCopiar.addEventListener("click", () => copiarDatos() );
-
-
-
-
-
-// const copiarDatos = () => { 
-   
-// }   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const desencriptarDatos = ( copiarDatos) => {
-
-//     desencriptar = copiarDatos.replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u")
-
-    
-// }
-
-
-
-    
-    
-
-
-
-
 
